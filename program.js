9e133fcefa2514f1f78e2da5ad349bfecde2eb1e
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-02-25 11:14:35 -0600

    Start work on making the code easier to read and develop with

--- program.js
+++ program.js
@@ -1,50 +1,50 @@
 // https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
 
+const regex = new RegExp("^([^;]*>)")
+
 window.onload = function()  {
+
+    var visitedPages = new Array()
+
     const app = document.getElementById('root')
     const container = document.createElement('div')
     container.setAttribute('class', 'container')
     app.appendChild(container)
 
-    var repoName = []
-    var repoDescription = []
-    var repoURL = []
     var request = new XMLHttpRequest()
 
-    request.open("GET", "https://api.github.com/users/benawad/repos", true)
-    request.onload = function()   {
-        response = JSON.parse(this.response)
-        response.forEach(index => {
-            if(request.status >= 305 && request.status < 400)   {
-                repoName.push("Error")
-                repoDescription.push("Error")
-                repoURL.push("Error")
-            }
-            else    {
-                const container = document.createElement("div")
-                const name = document.createElement("ol")
-                const description = document.createElement("ul")
-                const urlLink = document.createElement("a")
-                const url = document.createElement("ul")
-                
-                urlLink.setAttribute("href", index.html_url)
-                container.setAttribute("class", "container")
+    program("https://api.github.com/users/benawad/repos?per_page=100")
 
-                name.textContent = index.name
-                description.textContent = index.description
-                url.textContent = index.html_url
+    function program(url)   {
+        request.open("GET", url, true)
+        request.onload = function()   {
+            // TODO: Add error checking incase code 403 appears. Wanted code is 304
+            response = JSON.parse(this.response)
+            buildPage(response)
+        }
+        request.send()
+    }
 
-                name.appendChild(description)
-                urlLink.appendChild(url)
-                name.appendChild(urlLink)
-                container.appendChild(name)
-                app.appendChild(container)
-            }
+    function buildPage(json)    {
+        json.forEach(index =>   {
+            const container = document.createElement("div")
+            const name = document.createElement("ol")
+            const description = document.createElement("ul")
+            const urlLink = document.createElement("a")
+            const url = document.createElement("ul")
+            
+            urlLink.setAttribute("href", index.html_url)
+            container.setAttribute("class", "container")
+
+            name.textContent = index.name
+            description.textContent = index.description
+            url.textContent = index.html_url
+
+            name.appendChild(description)
+            urlLink.appendChild(url)
+            name.appendChild(urlLink)
+            container.appendChild(name)
+            app.appendChild(container)
         })
     }
-    request.send()
-
-    console.log(repoName)
-    console.log(repoDescription)
-    console.log(repoURL)
 }
