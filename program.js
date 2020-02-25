dd67f919285ab99278d65c88392a2bfd982ac5fb
Nicholas Synovic <nicholas.synovic@gmail.com>
2020-02-25 09:50:40 -0600

    Add error checking

--- program.js
+++ program.js
@@ -12,10 +12,17 @@ request.onload = function()   {
     response = JSON.parse(this.response)
 
     response.forEach(index => {
-        repoName.push(index.name)
-        repoDescription.push(index.description)
-        repoURL.push(index.html_url)
-    });
+        if(request.status >= 200 && request.status < 400)   {
+            repoName.push("Error")
+            repoDescription.push("Error")
+            repoURL.push("Error")
+        }
+        else    {
+            repoName.push(index.name)
+            repoDescription.push(index.description)
+            repoURL.push(index.html_url)
+        }
+    })
 }
 
 request.send()
