382eb68232c3825aefafd6f275e21b1f7ca46722
Nicholas M Synovic <nicholas.synovic@gmail.com>
2021-06-04 10:07:54 -0500

    Bug fixing and implementing logging

--- grd.js
+++ grd.js
@@ -1,42 +1,21 @@
-var request = new XMLHttpRequest()
-
 function grd(githubUsername) {
-  const githubRepositoryAPIURL = "https://api.github.com/users/" + githubUsername + "/repos?per_page=100"
+  var githubRepositoryAPIURL = "https://api.github.com/users/" + githubUsername + "/repos?per_page=100"
 
-  let repositoryData = []
-  getRepositories(githubRepositoryAPIURL, repositoryData)
-
-  return repositoryData
-
-  return repositoryData
+  return getRepositories(githubRepositoryAPIURL)
 }
 
 function getRepositories(githubRepositoryAPIURL, data) {
-  request.open("GET", githubRepositoryAPIURL, true)
-  request.onload = function () {
+  var data = []
+  var request = new XMLHttpRequest()
 
-    const jsonResponse = JSON.parse(this.response)
-    const nextPageURL = getNextPageURL(request.getResponseHeader("Link"))
+  request.open("GET", githubRepositoryAPIURL, true)
+  request.onload = function parseJSON() {
+
+    var jsonResponse = JSON.parse(this.response)
 
     jsonResponse.forEach(index => {
-      data.push([index.name, index.html_url, index.description])
+      console.log(index.name)
     })
-
-    if (nextPageURL != 0) {
-      getRepositories(nextPageURL, data)
-    }
-
-    return data
   }
-}
-
-function getNextPageURL(responseField) {
-  const splitURLArray = responseField.split(" ")
-  const nextURLIndex = splitURLArray.indexOf('rel=\"next\",')
-
-  if (nextURLIndex != -1) {
-    const url = splitURLArray[nextURLIndex - 1]
-    return url.substr(1, url.length - 3)
-  }
-  return 0
+  console.log(data)
 }
