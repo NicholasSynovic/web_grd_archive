15b2a3605395fcd007674fd1ec8deb09c7ea7a4c
Nicholas M Synovic <nicholas.synovic@gmail.com>
2021-06-04 09:50:38 -0500

    Move code from project.js to grd.ts

--- grd.ts
+++ grd.ts
@@ -5,34 +5,39 @@ function grd(githubUsername: String) {
   const githubRepositoryAPIURL = "https://api.github.com/users/" + githubUsername + "/repos?per_page=100"
 
   let repositoryData = []
-  repositoryData.push(getRepositories(githubRepositoryAPIURL, repositoryData))
+  getRepositories(githubRepositoryAPIURL, repositoryData)
+
+  return repositoryData
+
   return repositoryData
 }
 
-function getRepositories(githubRepositoryAPIURL: string, data: Array<string>) {
+function getRepositories(githubRepositoryAPIURL: string, data: Array<Array<string>>) {
   request.open("GET", githubRepositoryAPIURL, true)
   request.onload = function () {
+
     const jsonResponse = JSON.parse(this.response)
     const nextPageURL = getNextPageURL(request.getResponseHeader("Link"))
 
     jsonResponse.forEach(index => {
-      console.log("hello")
+      data.push([index.name, index.html_url, index.description])
     })
+
+    if (nextPageURL != 0) {
+      getRepositories(nextPageURL, data)
+    }
+
+    return data
   }
 }
 
-function getNextPageURL(responseField) {
-  if (responseField == null) {
-    return 0
-  }
-
+function getNextPageURL(responseField: string) {
   const splitURLArray = responseField.split(" ")
   const nextURLIndex = splitURLArray.indexOf('rel=\"next\",')
 
-  if (bar == -1) {
-    return 0
+  if (nextURLIndex != -1) {
+    const url = splitURLArray[nextURLIndex - 1]
+    return url.substr(1, url.length - 3)
   }
-
-  const url = splitURLArray[nextURLIndex - 1]
-  return url.substr(1, url.length - 3)
+  return 0
 }
